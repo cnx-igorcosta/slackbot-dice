6f99fd0fc4055c414eb738e2e3ad9dca562d520d
igorgcosta <igorgabriel@outlook.com>
2016-01-17 11:18:22 -0200

    correções do bot

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ bot.on('start', function() {
     };
 
     // define channel, where bot exist. You can adjust it there https://my.slack.com/services
-    //bot.postMessageToChannel('general', 'Rolagem de dados está no ar! \nPara rolar um dado digite  "dice" e o dado que você quer que eu role, exemplo: 3d6. \n Bom jogo! ', params);
-    bot.postMessageToChannel('general', 'teste dice', params);
+    bot.postMessageToChannel('general', 'Rolagem de dados está no ar! \nPara rolar um dado digite  "dice" e o dado que você quer que eu role, exemplo: "dice 3d6". \n Bom jogo! ', params);
+    //bot.postMessageToChannel('general', 'teste dice', params);
 
     // define existing username instead of 'user_name'
     bot.postMessageToUser('user_name', params.username, params);
 });
 
 bot.on('message', function(message){
-  console.log(message);
+  //console.log(message);
     if (isChatMessage(message) &&
           isChannelConversation(message) &&
           !isFromDiceBot(message) &&
@@ -33,34 +33,55 @@ bot.on('message', function(message){
 });
 
 var isChatMessage = function (message) {
-    console.log('isChatMessage : '+ (message.type === 'message' && Boolean(message.text)));
+    //console.log('isChatMessage : '+ (message.type === 'message' && Boolean(message.text)));
     return message.type === 'message' && Boolean(message.text);
 };
 
 var isChannelConversation = function (message) {
-    console.log('isChannelConversation : '+ (typeof message.channel === 'string' && message.channel[0] === 'C'));
+    //console.log('isChannelConversation : '+ (typeof message.channel === 'string' && message.channel[0] === 'C'));
     return typeof message.channel === 'string' && message.channel[0] === 'C';
 };
 
 var isFromDiceBot = function (message) {
-  console.log('isFromDiceBot : '+ (message.username === 'dice'));
+  //console.log('isFromDiceBot : '+ (message.username === 'dice'));
     //return message.user === bot.user.id;
     return message.username === bot.name;
 };
 
 var isMentioningDice = function (message) {
-  console.log('isMentioningDice : '+ (message.text.toLowerCase().indexOf('dice') > -1 || message.text.toLowerCase().indexOf(bot.name) > -1));
+  //console.log('isMentioningDice : '+ (message.text.toLowerCase().indexOf('dice') > -1 || message.text.toLowerCase().indexOf(bot.name) > -1));
     return message.text.toLowerCase().indexOf('dice') > -1 ||
         message.text.toLowerCase().indexOf(bot.name) > -1;
 };
 
 var rollDice = function(originalMessage) {
   var channel = getChannelById(originalMessage.channel);
-    bot.postMessageToChannel(channel.name, 'Rolou dado', {as_user: true});
+  var resultado = rolarDado(originalMessage.text);
+    bot.postMessageToChannel(channel.name, resultado, {as_user: true});
 }
 
 var getChannelById = function (channelId) {
     return bot.channels.filter(function (item) {
         return item.id === channelId;
     })[0];
+};
+
+var rolarDado = function(message){
+  var dado = message.replace(/ /g,'').replace(/(D|d)ice/g,'');
+  var resultado = "Resultado ";
+  if(/^\d+[d]\d+$/.test(dado)){
+    var vezes = parseInt(dado.substring(0, dado.indexOf('d')));
+    var faces = parseInt(dado.substring((dado.indexOf('d')+1),(dado.length)));
+    resultado = resultado + dado + " = ";
+    var total = 0;
+    for(var i = 0; i<(vezes); i++){
+      var valor = Math.floor((Math.random() * faces) + 1);
+      if(i > 0){
+       resultado = resultado + ", "
+      }
+      resultado = resultado + valor;
+      total = total+valor;
+    }
+  }
+  return resultado + "; Total: "+total;
 };
